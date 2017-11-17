e714339b9b85f85788f5a328c87f2387cc909aa1
Hamid Ghadyani <hamid@hamid.cc>
2017-11-17 03:59:31 -0500

    Add a preliminary Config module.

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use std::path::{PathBuf, Path};
+
+use serde_json;
+
+#[derive(Default,Debug,Serialize,Deserialize)]
+pub struct Config {
+//    /// Folder to store volatile data of the workflow
+//    workflow_cache_dir: PathBuf,
+//    /// Folder to store data of the workflow
+//    workflow_data_dir: PathBuf,
+    /// Which version of Alfred we are being executed under
+    alfred_version: String,
+    /// Number of bookmarks to show in Alfred
+    pins_to_show: usize,
+    /// Number of tags to show in Alfred
+    tags_to_show: usize,
+    /// Flag to perform search only on `tag` fields of bookmarks
+    tag_only_search: bool,
+    /// Flag to perform a fuzzy search
+    fuzzy_search: bool,
+    /// Flag to save bookmarks as private
+    private_new_pin: bool,
+    /// Flag to save bookmarks as toread
+    toread_new_pin: bool,
+    /// Flag to update cache after each bookmark saving automatically.
+    auto_update_cache: bool,
+    /// Authentication Token
+    auth_token: String,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn read_from<P: AsRef<Path>>(&mut self, p: P) -> Result<Config, String> {
+        Ok(Config::new())
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
 extern crate rusty_pin as rustypin;
 extern crate alfred;
 
 use std::io;
+use std::path::{Path, PathBuf};
+use std::env;
+use std::fs::File;
+use std::process;
+
+use rustypin::pinboard;
+
+mod config;
+
+use config::Config;
 
 fn main() {
-    alfred::json::write_items(
-        io::stdout(),
-        &[
-            alfred::Item::new("Item 1"),
-            alfred::ItemBuilder::new("Item 2")
-                .subtitle("Subtitle")
-                .into_item(),
-            alfred::ItemBuilder::new("Item 3")
-                .arg("Argument")
-                .subtitle("Subtitle")
-                .icon_filetype("public.folder")
-                .into_item(),
-        ],
-    );
+    let mut cache_dir = alfred::env::workflow_cache().unwrap_or_else(|| {
+        let mut dir = env::home_dir().unwrap_or(PathBuf::from(""));
+        dir.push(".cache");
+        dir.push("alfred-pinboard-rs");
+        dir
+    });
+    let mut data_dir = alfred::env::workflow_data().unwrap_or_else(|| {
+        let mut dir = env::home_dir().unwrap_or(PathBuf::from(""));
+        dir.push(".config");
+        dir.push("alfred-pinboard-rs");
+        dir
+    });
+    println!("{:?}", cache_dir);
+    data_dir.push("settings.json");
+
+    let mut config = Config::new().read_from(data_dir);
+    if let Err(_) = config {
+        process::exit(1);
+    } else {
+        println!("{:?}", config);
+    }
 }
+
