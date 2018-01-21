d7f3f3840fdd6bf13d63650c7f959e5bc1fc94ec
Hamid Ghadyani <hamid@hamid.cc>
2018-01-20 19:06:58 -0500

    Write errors to stdout instead of creating Alfred items.
    - This makes sense for update since Alfred will only show a notification
    rather than an item to inform user about the outcome of command.

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -3,15 +3,19 @@ use chrono::prelude::*;
 
 pub fn run(mut config: Config, mut pinboard: Pinboard) {
     match pinboard.is_cache_outdated(config.update_time) {
-        Err(err) => ::show_error_alfred(&err),
-        Ok(flag) => {
-            if flag {
+        Err(err) => {
+            io::stdout().write(format!("Error: {}", err).as_ref());
+            process::exit(1);
+        },
+        Ok(needs_update) => {
+            if needs_update {
                 pinboard.update_cache().unwrap_or_else(|err| {
-                    ::show_error_alfred(&err);
+                    io::stdout().write(format!("Error: {}", err).as_ref());
+                    process::exit(1);
                 });
                 config.update_time = Utc::now();
                 if let Err(_) = config.save() {
-                    ::show_error_alfred("Couldn't save to workflow's config file!");
+                    io::stdout().write(b"Error: Couldn't save update time to workflow's config file!");
                 }
                 io::stdout().write(b"Updated cache files!").unwrap();
             } else {
