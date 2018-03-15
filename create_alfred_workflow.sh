b4495e2bf3172cd9fd8946dba2f833b31b21a2ae
spamwax <1251233+spamwax@users.noreply.github.com>
2018-03-15 05:45:10 -0400

    Improve script that creates workflow's bundle.

--- create_alfred_workflow.sh
+++ create_alfred_workflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alfred_pinboard_rs="$HOME/src/learn/rust/alfred-pinboard-rs"
+alfred_pinboard_rs=`pwd`
 workflow_dir="$HOME/Dropbox/Alfred/Alfred.alfredpreferences/workflows/user.workflow.665EAB20-5141-463D-8C5A-90093EEAA756"
 
 echo "Building new release..."
