a1d70eae856743e13c4d237fc328709ecb790921
Hamid Ghadyani <hamid@hamid.cc>
2018-04-04 03:26:31 -0400

    Attempt at copying binary to stage folder on travis.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,30 +18,26 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --target "$TARGET" --release -- -C lto
+    cross rustc --bin alfred-pinboard-rs --target "$TARGET" --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    create_workflow "$stage"
+    res_dir="$src/res/workflow"
+
+    # echo "Copying executable to workflow's folder..."
+    cp "$src/target/release/alfred-pinboard-rs" "$stage"
+    cp "$res_dir"/* "$stage"
+
+    # echo "Creating the workflow bundle..."
+    cd "$stage" || exit
+    rm -f AlfredPinboardRust.alfredworkflow
+
+    zip -r AlfredPinboardRust.alfredworkflow ./*
 
     cd "$stage"
     tar czf "$src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz" ./AlfredPinboardRust.alfredworkflow
     cd "$src"
 
     rm -rf "$stage"
-}
-
-create_workflow() {
-    res_dir="res/workflow"
-
-    # echo "Copying executable to workflow's folder..."
-    cp target/release/alfred-pinboard-rs "$1"
-    cp "$res_dir"/* "$1"
-
-    # echo "Creating the workflow bundle..."
-    cd "$1" || exit
-    rm -f AlfredPinboardRust.alfredworkflow
-
-    zip -r AlfredPinboardRust.alfredworkflow ./*
 }
 
 main
