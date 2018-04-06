e70b4544bebb6a95f41f7322803700a7329c1a27
Hamid Ghadyani <hamid@hamid.cc>
2018-04-06 02:15:38 -0400

    Automatically latest tag from git to info.plist

--- create_alfred_workflow.sh
+++ create_alfred_workflow.sh
@@ -3,7 +3,7 @@
 set -x
 alfred_pinboard_rs=$(pwd)
 workflow_dir="$HOME/Dropbox/Alfred/Alfred.alfredpreferences/workflows/user.workflow.665EAB20-5141-463D-8C5A-90093EEAA756"
-res_dir="./res/workflow"
+res_dir="$alfred_pinboard_rs/res/workflow"
 
 echo "Building new release..."
 cd "$alfred_pinboard_rs" || exit
@@ -15,6 +15,11 @@ cp "$workflow_dir"/* "$res_dir"
 echo "Copying executable to workflow's folder..."
 strip target/release/alfred-pinboard-rs
 cp target/release/alfred-pinboard-rs "$res_dir"
+
+echo "Updating version in info.plist"
+version=$(git describe --tags --abbrev=0)
+defaults write "$res_dir"/info.plist version "$version"
+plutil -convert xml1 "$res_dir"/info.plist
 
 echo "Creating the workflow bundle..."
 cd "$res_dir" || exit
