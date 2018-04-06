a30f2c2a8606a6807aa9b321315a5ed13e047315
Hamid Ghadyani <hamid@hamid.cc>
2018-04-06 01:52:34 -0400

    Update create_alfred_workflow.
    - Copy resources from actual workflow folder to res folder before
    creating a new zip file.
    - Update info.plist: Add workflow version

--- create_alfred_workflow.sh
+++ create_alfred_workflow.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-alfred_pinboard_rs=`pwd`
+set -x
+alfred_pinboard_rs=$(pwd)
 workflow_dir="$HOME/Dropbox/Alfred/Alfred.alfredpreferences/workflows/user.workflow.665EAB20-5141-463D-8C5A-90093EEAA756"
+res_dir="./res/workflow"
 
 echo "Building new release..."
 cd "$alfred_pinboard_rs" || exit
 cargo build --release > build.log 2>&1
 
+echo "Copying resoursces from Alfred's workflow dir..."
+cp "$workflow_dir"/* "$res_dir"
+
 echo "Copying executable to workflow's folder..."
-cp target/release/alfred-pinboard-rs "$workflow_dir"
+strip target/release/alfred-pinboard-rs
+cp target/release/alfred-pinboard-rs "$res_dir"
 
 echo "Creating the workflow bundle..."
-cd "$workflow_dir" || exit
+cd "$res_dir" || exit
 rm -f AlfredPinboardRust.alfredworkflow
 
-zip -r AlfredPinboardRust.alfredworkflow ./* || exit
+zip -r AlfredPinboardRust.alfredworkflow ./*
 
 echo "Moving bundle to executable folder..."
 mv AlfredPinboardRust.alfredworkflow "$alfred_pinboard_rs"
+rm alfred-pinboard-rs
