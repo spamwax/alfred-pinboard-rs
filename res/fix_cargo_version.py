558ea04b73c5c96f4a696220bd62196b62017a7d
Hamid Ghadyani <hamid@hamid.cc>
2019-07-11 04:34:12 -0400

    Minor linting.

--- res/fix_cargo_version.py
+++ res/fix_cargo_version.py
@@ -2,7 +2,7 @@ import toml
 import sys
 
 c = toml.load("Cargo.toml")
-c['package']['version'] =  sys.argv[1]
+c['package']['version'] = sys.argv[1]
 c['profile'] = {'release': {'lto': True}}
 
 f = open("Cargo.toml", "w")
