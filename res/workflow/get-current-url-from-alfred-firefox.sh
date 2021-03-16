7dc5d8688e0eeaecd9313417bab9b79d6fca18f3
Brett Bender <brett.bender@rover.com>
2021-03-16 14:25:02 -0700

    check that alfred-firefox returned a URL

--- res/workflow/get-current-url-from-alfred-firefox.sh
+++ res/workflow/get-current-url-from-alfred-firefox.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
+#
+# Get the current URL from Firefox via the alfred-firefox workflow
+#
+# If that workflow is not installed, exits with no output;
+# then we fallback to grabbing the URL via clipboard
 
 ALFRED_FIREFOX="$(ls ../*/alfred-firefox | head -1)"
 if [[ -z "$ALFRED_FIREFOX" || ! -x "$ALFRED_FIREFOX" ]] ; then
@@ -6,4 +11,9 @@ if [[ -z "$ALFRED_FIREFOX" || ! -x "$ALFRED_FIREFOX" ]] ; then
 fi
 
 eval `"$ALFRED_FIREFOX" tab-info -shell`
+
+if [[ -z "$FF_URL" ]] ; then
+    exit
+fi
+
 echo "$FF_URL" fd850fc2e63511e79f720023dfdf24ec "$FF_TITLE"
