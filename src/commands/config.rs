6f8018dbb26b534031ea01e81459acc1d5d62be2
Hamid Ghadyani <hamid@hamid.cc>
2022-07-10 18:57:47 -0400

    'pconf' can now output both json & xml

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,4 +1,5 @@
-use super::{io, process, Config, SubCommand};
+use super::{process, Config, SubCommand};
+use alfred::ItemBuilder;
 use chrono::prelude::Local;
 
 pub fn run(x: SubCommand) {
@@ -42,6 +43,7 @@ pub fn run(x: SubCommand) {
                 }
                 config
             });
+            debug!("{:?}", config);
             config.auth_token.update(auth_token);
             config.pins_to_show.update(number_pins);
             config.tags_to_show.update(number_tags);
@@ -75,80 +77,75 @@ pub fn run(x: SubCommand) {
 
 fn show_config(config: &Config) {
     debug!("Starting in show_config");
-    // TODO: Add support for Alfred 2 by returning XML <09-02-18, Hamid> //
-    // If Using Alfred Version >=3
-    if config.can_use_json() {
-        use alfred::ItemBuilder;
-        alfred::json::Builder::with_items(&[
-            ItemBuilder::new("Only search tags")
-                .subtitle(format!("{:?}", config.tag_only_search))
-                .arg("pset tagonly")
-                .icon_path("tagonly.png")
-                .into_item(),
-            ItemBuilder::new("Use fuzzy search")
-                .subtitle(format!("{:?}", config.fuzzy_search))
-                .arg("pset fuzzy")
-                .icon_path("fuzzy.png")
-                .into_item(),
-            ItemBuilder::new("Automatically update cache")
-                .subtitle(format!("{:?}", config.auto_update_cache))
-                .arg("pset auto")
-                .icon_path("auto_update_cache.png")
-                .into_item(),
-            ItemBuilder::new("Suggest popular tags for open browser tab")
-                .subtitle(format!("{:?}", config.suggest_tags))
-                .arg("pset suggest_tags")
-                .icon_path("suggest.png")
-                .into_item(),
-            ItemBuilder::new("Mark new bookmarks as toread")
-                .subtitle(format!("{:?}", config.toread_new_pin))
-                .arg("pset toread")
-                .icon_path("toread.png")
-                .into_item(),
-            ItemBuilder::new("Mark new bookmarks as private")
-                .subtitle(format!("{:?}", config.private_new_pin))
-                .arg("pset shared")
-                .icon_path("private.png")
-                .into_item(),
-            ItemBuilder::new("Check if page is bookmarked")
-                .subtitle(format!("{:?}", config.page_is_bookmarked))
-                .arg("pset check_bookmarked")
-                .icon_path("check_bookmarked_page.png")
-                .into_item(),
-            ItemBuilder::new("Show TAGs vs URLs in search results")
-                .subtitle(format!("{:?}", config.show_url_vs_tags))
-                .arg("pset url_tag")
-                .icon_path("url.png")
-                .into_item(),
-            ItemBuilder::new("Number of tags to show")
-                .subtitle(format!("{:?}", config.tags_to_show))
-                .arg("pset tags")
-                .icon_path("no_of_tags.png")
-                .into_item(),
-            ItemBuilder::new("Number of bookmarks to show")
-                .subtitle(format!("{:?}", config.pins_to_show))
-                .arg("pset bookmarks")
-                .icon_path("no_of_pins.png")
-                .into_item(),
-            ItemBuilder::new("Click to check for Workflow updates.")
-                .arg("pcheck")
-                .icon_path("check_update.png")
-                .into_item(),
-            ItemBuilder::new(
-                config
-                    .update_time
-                    .with_timezone(&Local)
-                    .format("%Y-%m-%d %H:%M:%S")
-                    .to_string(),
-            )
-            .subtitle("Latest cache update")
-            .arg("pupdate")
-            .icon_path("auto_update.png")
+    let items = vec![
+        ItemBuilder::new("Only search tags")
+            .subtitle(format!("{:?}", config.tag_only_search))
+            .arg("pset tagonly")
+            .icon_path("tagonly.png")
             .into_item(),
-        ])
-        .write(io::stdout())
-        .expect("Couldn't build alfred items");
-    }
+        ItemBuilder::new("Use fuzzy search")
+            .subtitle(format!("{:?}", config.fuzzy_search))
+            .arg("pset fuzzy")
+            .icon_path("fuzzy.png")
+            .into_item(),
+        ItemBuilder::new("Automatically update cache")
+            .subtitle(format!("{:?}", config.auto_update_cache))
+            .arg("pset auto")
+            .icon_path("auto_update_cache.png")
+            .into_item(),
+        ItemBuilder::new("Suggest popular tags for open browser tab")
+            .subtitle(format!("{:?}", config.suggest_tags))
+            .arg("pset suggest_tags")
+            .icon_path("suggest.png")
+            .into_item(),
+        ItemBuilder::new("Mark new bookmarks as toread")
+            .subtitle(format!("{:?}", config.toread_new_pin))
+            .arg("pset toread")
+            .icon_path("toread.png")
+            .into_item(),
+        ItemBuilder::new("Mark new bookmarks as private")
+            .subtitle(format!("{:?}", config.private_new_pin))
+            .arg("pset shared")
+            .icon_path("private.png")
+            .into_item(),
+        ItemBuilder::new("Check if page is bookmarked")
+            .subtitle(format!("{:?}", config.page_is_bookmarked))
+            .arg("pset check_bookmarked")
+            .icon_path("check_bookmarked_page.png")
+            .into_item(),
+        ItemBuilder::new("Show TAGs vs URLs in search results")
+            .subtitle(format!("{:?}", config.show_url_vs_tags))
+            .arg("pset url_tag")
+            .icon_path("url.png")
+            .into_item(),
+        ItemBuilder::new("Number of tags to show")
+            .subtitle(format!("{:?}", config.tags_to_show))
+            .arg("pset tags")
+            .icon_path("no_of_tags.png")
+            .into_item(),
+        ItemBuilder::new("Number of bookmarks to show")
+            .subtitle(format!("{:?}", config.pins_to_show))
+            .arg("pset bookmarks")
+            .icon_path("no_of_pins.png")
+            .into_item(),
+        ItemBuilder::new("Click to check for Workflow updates.")
+            .arg("pcheck")
+            .icon_path("check_update.png")
+            .into_item(),
+        ItemBuilder::new(
+            config
+                .update_time
+                .with_timezone(&Local)
+                .format("%Y-%m-%d %H:%M:%S")
+                .to_string(),
+        )
+        .subtitle("Latest cache update")
+        .arg("pupdate")
+        .icon_path("auto_update.png")
+        .into_item(),
+    ];
+    let none: Option<Vec<(&str, &str)>> = None;
+    crate::write_to_alfred(items, config.can_use_json(), none);
 }
 
 /// Trait to update a value optionally based on `opt`
