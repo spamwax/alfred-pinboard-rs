a265acd4d5997a0794a200cb7cdc30a0d79d86c8
Hamid Ghadyani <hamid@hamid.cc>
2023-06-24 19:05:59 -0400

    Create alfred_workflow_data

--- .github/workflows/run_tests_win.bat
+++ .github/workflows/run_tests_win.bat
@@ -7,13 +7,14 @@ set alfred_workflow_version=0.11.1
 set alfred_workflow_uid=hamid63
 set alfred_workflow_name=RustyPin
 set alfred_workflow_bundleid="cc.hamid.alfred-pinboard-rs"
-set alfred_workflow_data="%GITHUB_WORKSPACE%\data_cache"
-set alfred_workflow_cache="%GITHUB_WORKSPACE%\data_cache"
+set alfred_workflow_data=%GITHUB_WORKSPACE%\data_cache
+set alfred_workflow_cache=%GITHUB_WORKSPACE%\data_cache
 
 REM set TARGET=x86_64-pc-windows-msvc
 set working_dir="%GITHUB_WORKSPACE%"
 
 dir %working_dir%
+mkdir %alfred_workflow_data%
 
 cargo build && ^
 cargo run -- config --authorization hamid:12345 && ^
