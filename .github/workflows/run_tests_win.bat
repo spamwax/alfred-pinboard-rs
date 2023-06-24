1fefec4b43646d60fcc25ba9c9110b106ab9cf35
Hamid Ghadyani <hamid@hamid.cc>
2023-06-24 17:34:38 -0400

    First try @ running windows tests on github

---
+++ .github/workflows/run_tests_win.bat
@@ -0,0 +1,20 @@
+set RUST_BACKTRACE=1
+set alfred_debug=1
+set alfred_version=4.0.1
+set alfred_workflow_version=0.11.1
+set alfred_workflow_uid=hamid63
+set alfred_workflow_name=RustyPin
+set alfred_workflow_bundleid="cc.hamid.alfred-pinboard-rs"
+set alfred_workflow_data="%GITHUB_WORKSPACE%\data_cache"
+set alfred_workflow_cache="%GITHUB_WORKSPACE%\data_cache"
+
+REM set TARGET=x86_64-pc-windows-msvc
+set working_dir="%GITHUB_WORKSPACE%"
+mkdir "%GITHUB_WORKSPACE%"
+
+cargo build &&
+cargo run -- config --authorization hamid:12345 &&
+cargo run -- config -d &&
+cargo test -- --nocapture --test-threads=1
+
+
