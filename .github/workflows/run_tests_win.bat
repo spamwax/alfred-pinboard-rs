c5b0d7c885d61d1ae3b87ca367bf6e90af5c2b8a
Hamid Ghadyani <hamid@hamid.cc>
2023-06-25 18:43:44 -0400

    Fix missing $ in windows.yml
    - Run executable directly without using cargo & do tests using real data
      from pinboard.in

--- .github/workflows/run_tests_win.bat
+++ .github/workflows/run_tests_win.bat
@@ -18,7 +18,31 @@ set working_dir="%GITHUB_WORKSPACE%"
 dir %working_dir%
 mkdir %alfred_workflow_data%
 
-cargo build && ^
-cargo run -- config --authorization hamid:12345 && ^
-cargo run -- config -d && ^
+%executable% config --authorization hamid:12345
+%executable% config --display
+
 cargo test -- --nocapture --test-threads=1
+
+%executable% config --authorization %PINBOARD_TOKEN%
+%executable% update
+powershell -nop -c "& {sleep 2}"
+%executable% search -U rust async
+
+dir %alfred_workflow_data%
+dir %alfred_workflow_cache%
+rmdir /s /q %alfred_workflow_data%
+
+
+echo %GITHUB_REF_NAME% > tmp.txt
+
+Rem Turns the echo off so that each command will not be shown when executed
+@echo off
+echo "Hello World"
+
+Rem Displays the contents of the PATH variable
+echo %PATH%
+
+Rem Turns the echo on so that each command will be shown as executed
+echo on
+echo "ON Hello World"
+
