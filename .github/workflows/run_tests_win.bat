d3940b16896936c5fa64e5356a8e946d47b5eba0
Hamid Ghadyani <hamid@hamid.cc>
2023-06-25 18:59:24 -0400

    Improve run_tests_win.bat to become more like linux version

--- .github/workflows/run_tests_win.bat
+++ .github/workflows/run_tests_win.bat
@@ -2,6 +2,8 @@ set executable=%1
 
 echo %executable%
 
+Rem Turns the echo off so that each command will not be shown when executed
+@echo off
 set RUST_BACKTRACE=1
 set alfred_debug=1
 set alfred_version=4.0.1
@@ -15,34 +17,30 @@ set alfred_workflow_cache=%GITHUB_WORKSPACE%\data_cache
 REM set TARGET=x86_64-pc-windows-msvc
 set working_dir="%GITHUB_WORKSPACE%"
 
+echo on
+
 dir %working_dir%
 mkdir %alfred_workflow_data%
 
-%executable% config --authorization hamid:12345
-%executable% config --display
 
-cargo test -- --nocapture --test-threads=1
+%executable% config --display
+powershell -nop -c "& {sleep 4}"
 
 %executable% config --authorization %PINBOARD_TOKEN%
+powershell -nop -c "& {sleep 4}"
+
 %executable% update
-powershell -nop -c "& {sleep 2}"
+powershell -nop -c "& {sleep 4}"
+
 %executable% search -U rust async
+powershell -nop -c "& {sleep 4}"
 
 dir %alfred_workflow_data%
-dir %alfred_workflow_cache%
 rmdir /s /q %alfred_workflow_data%
 
 
 echo %GITHUB_REF_NAME% > tmp.txt
+grep -E '[0-9]+/merge' tmp.txt
 
-Rem Turns the echo off so that each command will not be shown when executed
-@echo off
-echo "Hello World"
-
-Rem Displays the contents of the PATH variable
-echo %PATH%
-
-Rem Turns the echo on so that each command will be shown as executed
-echo on
-echo "ON Hello World"
-
+echo "23/merge" > tmp.txt
+grep -E '[0-9]+/merge' tmp.txt
