83ba6dfde04d7f46fbbab042bf6493a79901a4af
Hamid Ghadyani <hamid@hamid.cc>
2023-06-25 21:58:06 -0400

    Check for PR in run_tests_win.bat
    - and run a simple test vs a full test using real data.

--- .github/workflows/run_tests_win.bat
+++ .github/workflows/run_tests_win.bat
@@ -6,8 +6,8 @@ Rem Turns the echo off so that each command will not be shown when executed
 @echo off
 set RUST_BACKTRACE=1
 set alfred_debug=1
-set alfred_version=4.0.1
-set alfred_workflow_version=0.11.1
+set alfred_version=4.5.1
+set alfred_workflow_version=0.16.1
 set alfred_workflow_uid=hamid63
 set alfred_workflow_name=RustyPin
 set alfred_workflow_bundleid="cc.hamid.alfred-pinboard-rs"
@@ -22,25 +22,29 @@ echo on
 dir %working_dir%
 mkdir %alfred_workflow_data%
 
-
-%executable% config --display
-powershell -nop -c "& {sleep 4}"
-
-%executable% config --authorization %PINBOARD_TOKEN%
-powershell -nop -c "& {sleep 4}"
-
-%executable% update
-powershell -nop -c "& {sleep 4}"
-
-%executable% search -U rust async
-powershell -nop -c "& {sleep 4}"
-
-dir %alfred_workflow_data%
-rmdir /s /q %alfred_workflow_data%
-
-
 echo %GITHUB_REF_NAME% > tmp.txt
 grep -E '[0-9]+/merge' tmp.txt
 
-echo "23/merge" > tmp.txt
-grep -E '[0-9]+/merge' tmp.txt
+if %ERRORLEVEL% EQU 0 (
+    echo "This is a pull request"
+    PINBOARD_TOKEN="hamid:123456"
+    %executable% config --authorization %PINBOARD_TOKEN%
+    powershell -nop -c "& {sleep 4}"
+ ) else (
+   echo "This is not a pull request"
+   %executable% config --authorization %PINBOARD_TOKEN%
+   powershell -nop -c "& {sleep 4}"
+
+   %executable% update
+   powershell -nop -c "& {sleep 4}"
+
+   %executable% search -U rust async
+   powershell -nop -c "& {sleep 4}"
+ )
+
+ %executable% config --display
+ powershell -nop -c "& {sleep 4}"
+
+
+dir %alfred_workflow_data%
+rmdir /s /q %alfred_workflow_data%
